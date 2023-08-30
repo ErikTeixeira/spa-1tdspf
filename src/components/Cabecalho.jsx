0a07e7ef8467becc747b5dbf951b5094556ce132
ErikTeixeira <erik.barbosa515@gmail.com>
2023-08-30 12:00:04 -0300

    react

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -2,7 +2,7 @@ export default function Cabecalho() {
     return (
       <>
         <header>
-          <h1>Vite + React / Coded By - RM552178</h1>
+          <h1>Vite + React / Coded By - RM551377</h1>
           <ul>
             <li>
               <a href="#">Item - 1</a>
---
+++ src/routes/Produtos.jsx
@@ -0,0 +1,9 @@
+export default function Produtos() {
+    // rfc - cria tudo
+    return (
+      <>
+          <h1>Produtos</h1>
+      </>
+    )
+  }
+  
