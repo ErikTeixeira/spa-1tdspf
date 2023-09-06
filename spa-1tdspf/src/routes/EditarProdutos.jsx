5e5a00ecaee8ba343518ccc792eeafa6ba97043c
ErikTeixeira <erik.barbosa515@gmail.com>
2023-09-06 12:12:44 -0300

    formulario editar item

--- spa-1tdspf/src/routes/EditarProdutos.jsx
+++ spa-1tdspf/src/routes/EditarProdutos.jsx
@@ -1,27 +1,41 @@
-import { useParams } from "react-router-dom";
-import { ListaProdutos } from "../components/ListaProdutos";
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { ListaProdutos } from '../components/ListaProdutos';
 
 export default function EditarProdutos() {
-  //Recuperando o id que chega no request através do PATH.
-  //Vamos urtilizar o Hook useParams().
 
   const {id} = useParams();
 
-  const produtoRecuperado = ListaProdutos.filter((produto) => {
-     if(produto.id == id){
-        return produto;
-     }
-  });
+  const produtoRecuperado = ListaProdutos.filter((produto) => produto.id == id)[0]
 
-  const objProdutoRecuperado = produtoRecuperado[0];
 
+      
   return (
     <>
-      <h1>EditarProdutos</h1>
-      <h2>PRODUTO</h2>
-      <p>NOME : {objProdutoRecuperado.nome}</p>
-      <p>DESCRIÇÃO : {objProdutoRecuperado.desc}</p>
-      <p>PREÇO : {objProdutoRecuperado.preco}</p>
+        <div>
+          <form>
+            <fieldset>
+              <legend>Produto Selecionado</legend>
+              <div>
+                <label htmlFor="idNome">Nome</label>
+                <input type="text" name="nome" id="idNome" defaultValue={produtoRecuperado.nome}/>
+              </div>
+              <div>
+                <label htmlFor="idDesc">Descrição</label>
+                <input type="text" name="desc" id="idDesc" defaultValue={produtoRecuperado.desc}/>
+              </div>
+              <div>
+                <label htmlFor="idPreco">Preço</label>
+                <input type="text" name="preco" id="idPreco" defaultValue={produtoRecuperado.preco}/>
+              </div>
+              <div>
+                <button>EDITAR</button>
+              </div>
+            </fieldset>
+          </form>
+        </div>
     </>
-  );
+    
+  )
 }
+  
